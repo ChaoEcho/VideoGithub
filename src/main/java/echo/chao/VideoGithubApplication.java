48e602616368716b47649b5d041e6af8c3cd93a3
echochao <echochao@126.com>
2023-02-16 17:35:24 +0800

    初始化项目，并测试

---
+++ src/main/java/echo/chao/VideoGithubApplication.java
@@ -0,0 +1,13 @@
+package echo.chao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/echo/chao/VideoGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package echo.chao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
